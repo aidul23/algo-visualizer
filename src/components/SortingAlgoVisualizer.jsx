7d79a2840aa24375c828c01fe2aff99b5ea4f80b
Md. Aidul Islam <aidul5613@gmail.com>
2024-02-27 02:31:00 +0200

    basic sort done with ui update

--- src/components/SortingAlgoVisualizer.jsx
+++ src/components/SortingAlgoVisualizer.jsx
@@ -1,24 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function SortingAlgoVisualizer(props) {
+  const [bars, setBars] = useState([]);
 
-    const[bars, setBars] = useState([2,4,2,7,4,3,7,6]);
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-    console.log(bars);
+  const generateArray = () => {
+    const bars = [];
+    for (let i = 0; i < 100; i++) {
+      bars.push(randomIntFromInterval(5, 400));
+    }
+    setBars(bars);
+  };
 
-    return (
-            <div className='bar-chart'>
-                {
-                    bars.map((value,index) => (
-                        <div 
-                            key={index}
-                            className='bar'
-                            style={{ height: `${value * 10}px`}}
-                        />
-                    ))
-                }
-            </div>
-    );
+  const sortArr = () => {
+    const sortedArr = [...bars];
+    sortedArr.sort((a,b) => a-b);
+    setBars(sortedArr);
+  };
+
+  useEffect(() => {
+    generateArray();
+  }, []);
+
+
+  return (
+    <div className="container">
+      <div className="bar-chart">
+        {bars.map((value, index) => (
+          <div key={index} className="bar" style={{ height: `${value}px` }} />
+        ))}
+        <div className="buttons">
+          <button onClick={generateArray} className="btn">
+            Generate Array
+          </button>
+          <button onClick={sortArr} className="btn">
+            Bubble Sort
+          </button>
+          <button onClick={sortArr} className="btn">
+            Sort
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SortingAlgoVisualizer;
