151efed00ec5b3e6dda3bd1a1cfe5ae2471d0782
Md. Aidul Islam <aidul5613@gmail.com>
2024-02-27 03:12:11 +0200

    bubble sort implemented

--- src/components/SortingAlgoVisualizer.jsx
+++ src/components/SortingAlgoVisualizer.jsx
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from "react";
 function SortingAlgoVisualizer(props) {
   const [bars, setBars] = useState([]);
 
+  useEffect(() => {
+    generateArray();
+  }, []);
+
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
@@ -15,15 +19,41 @@ function SortingAlgoVisualizer(props) {
     setBars(bars);
   };
 
+//   function swap(arr, index1, index2) {
+//     if (index1 < 0 || index1 >= arr.length || index2 < 0 || index2 >= arr.length) {
+//         return arr;
+//     }
+
+//     [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+
+//     return arr;
+// }
+
   const sortArr = () => {
     const sortedArr = [...bars];
     sortedArr.sort((a,b) => a-b);
     setBars(sortedArr);
   };
 
-  useEffect(() => {
-    generateArray();
-  }, []);
+  const bubbleSort = () => {
+    const arrForBubbleSort = [...bars];
+    let isSwapped = false;
+
+    for(let i = 0 ; i < arrForBubbleSort.length - 1 ; i++) {
+        for(let j = 0 ; j < arrForBubbleSort.length - i -1 ; j++) {
+            if(arrForBubbleSort[j] > arrForBubbleSort[j + 1]) {
+                [arrForBubbleSort[j], arrForBubbleSort[j+1]] = [arrForBubbleSort[j+1], arrForBubbleSort[j]];
+                isSwapped = true;
+            }
+        }
+
+        if (isSwapped === false)
+            break;
+    }
+    setBars(arrForBubbleSort);
+  };
+
+  
 
 
   return (
@@ -36,7 +66,7 @@ function SortingAlgoVisualizer(props) {
           <button onClick={generateArray} className="btn">
             Generate Array
           </button>
-          <button onClick={sortArr} className="btn">
+          <button onClick={bubbleSort} className="btn">
             Bubble Sort
           </button>
           <button onClick={sortArr} className="btn">
